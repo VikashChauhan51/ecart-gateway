bbd7249db9938a4a31b371913a02e24925a8341a
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-06-19 15:16:05 +0530

    implemented timeout policy

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,15 +1,32 @@
+using ApiGateway;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 
 // Add services to the container.
+builder.Services.AddSingleton<PollyPolicies>();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRequestTimeouts(options =>
+{
+    options.AddPolicy("customTimeoutPolicy", TimeSpan.FromSeconds(30));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customCorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin();
+    });
+});
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-
+app.UseRequestTimeouts();
+app.UseCors();
 app.MapReverseProxy();
 
 app.Run();
